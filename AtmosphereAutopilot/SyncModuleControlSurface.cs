69c7ed740c3c60100e18499a38706576d2dc07fc
Boris-Barboris <ismailsiege@gmail.com>
2016-04-26 16:35:38 +0300

    support of exponential stock csurface

--- AtmosphereAutopilot/SyncModuleControlSurface.cs
+++ AtmosphereAutopilot/SyncModuleControlSurface.cs
@@ -66,25 +66,35 @@ namespace AtmosphereAutopilot
 
             float spd_factor = TimeWarp.fixedDeltaTime * CSURF_SPD;
             float fwd_airstream_factor = Mathf.Sign(Vector3.Dot(vessel.ReferenceTransform.up, vessel.srf_velocity) + 0.1f);
+            float exp_spd_factor = actuatorSpeed / actuatorSpeedNormScale * TimeWarp.fixedDeltaTime;
 
             if (!ignorePitch)
             {
                 float axis_factor = Vector3.Dot(vessel.ReferenceTransform.right, baseTransform.right) * fwd_airstream_factor;
                 float new_pitch_action = pitch_input * axis_factor * Math.Sign(Vector3.Dot(world_com - baseTransform.position, vessel.ReferenceTransform.up));
-                prev_pitch_action = prev_pitch_action + Common.Clampf(new_pitch_action - prev_pitch_action, spd_factor * Math.Abs(axis_factor));
+                if (useExponentialSpeed)
+                    prev_pitch_action = Mathf.Lerp(prev_pitch_action, new_pitch_action, exp_spd_factor);
+                else
+                    prev_pitch_action = prev_pitch_action + Common.Clampf(new_pitch_action - prev_pitch_action, spd_factor * Math.Abs(axis_factor));
             }
             if (!ignoreRoll)
             {
                 float axis_factor = Vector3.Dot(vessel.ReferenceTransform.up, baseTransform.up) * fwd_airstream_factor;
                 float new_roll_action = roll_input * axis_factor * Math.Sign(Vector3.Dot(vessel.ReferenceTransform.up,
                     Vector3.Cross(world_com - baseTransform.position, baseTransform.forward)));
-                prev_roll_action = prev_roll_action + Common.Clampf(new_roll_action - prev_roll_action, spd_factor * axis_factor);
+                if (useExponentialSpeed)
+                    prev_roll_action = Mathf.Lerp(prev_roll_action, new_roll_action, exp_spd_factor);
+                else
+                    prev_roll_action = prev_roll_action + Common.Clampf(new_roll_action - prev_roll_action, spd_factor * axis_factor);
             }
             if (!ignoreYaw)
             {
                 float axis_factor = Vector3.Dot(vessel.ReferenceTransform.forward, baseTransform.right) * fwd_airstream_factor;
                 float new_yaw_action = yaw_input * axis_factor * Math.Sign(Vector3.Dot(world_com - baseTransform.position, vessel.ReferenceTransform.up));
-                prev_yaw_action = prev_yaw_action + Common.Clampf(new_yaw_action - prev_yaw_action, spd_factor * Math.Abs(axis_factor));
+                if (useExponentialSpeed)
+                    prev_yaw_action = Mathf.Lerp(prev_yaw_action, new_yaw_action, exp_spd_factor);
+                else
+                    prev_yaw_action = prev_yaw_action + Common.Clampf(new_yaw_action - prev_yaw_action, spd_factor * Math.Abs(axis_factor));
             }
 
             deflection = action = Common.Clampf(prev_pitch_action + prev_roll_action + prev_yaw_action, 1.0f);
